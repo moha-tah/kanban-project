696da0420cbf4d35e0915b39956df7a4e585e61f
Karim Abdelkafi <abdelkafikarim81@gmail.com>
2025-10-22 10:57:42 +0200

    [+] ajout ComCallsDataClient

--- root/client/src/interfaces/ComCallsDataClient.java
+++ root/client/src/interfaces/ComCallsDataClient.java
@@ -1,9 +1,9 @@
 package interfaces ;
 
-import common.src.dataClasses.Kanban;
-import common.src.dataClasses.LightKanban;
-import common.src.dataClasses.LightUser;
-import common.src.dataClasses.Modification;
+import root.common.src.dataClasses.Kanban;
+import root.common.src.dataClasses.LightUser;
+import root.common.src.dataClasses.LightKanban;
+import root.common.src.dataClasses.Modification;
 
 import java.util.List;
 import java.util.UUID;
