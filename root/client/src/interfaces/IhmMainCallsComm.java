2c8f468446b9ba67798b36700d5181ab7a576711
mbideaus <maxence.bideau-sorita@etu.utc.fr>
2025-10-22 12:33:49 +0200

    Removed server messages in interfaces

--- root/client/src/interfaces/IhmMainCallsComm.java
+++ root/client/src/interfaces/IhmMainCallsComm.java
@@ -9,10 +9,11 @@ public interface IhmMainCallsComm {
     void sendPermissionResponse(UUID LightUserId, UUID LightKanbanId, boolean accepted);
     void connectToServer(UUID LightUserId, List<LightKanban> listKanbans);
     void connectionRequest(LightUser LightUser, List<LightKanban> listKanbans);
-    void closingKanbanToServer(UUID LightUserId);
-    void forwardRequestModification(UUID LightUserId, UUID CardId, STATUS);
     void notifyDecision(UUID LightUserId, UUID LightKanbanId, boolean accepted);
     void notifyEditions(LightKanban LightKanban);
     void askKanban(UUID LightKanbanId);
-    Kanban getKanban(UUID LightKanbanId, UUID LightUserId);   
+    void getKanban(UUID LightKanbanId);   
+
+
+    void sendRequestModification(LightUser LightUser, UUID CardId, newStatus);
 }
