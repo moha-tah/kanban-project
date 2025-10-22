c43b1592956278cbbae8975489948617430b394f
Engel <engelcalon@gmail.com>
2025-10-22 10:18:10 +0200

    [+] MainCallsData

---
+++ root/client/src/interfaces/MainCallsData.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.UUID;
+
+public interface MainCallsData {
+
+    void saveUser();
+
+    AuthResult authentify(String username, String password);
+
+    LightUser getLightUser();
+
+    List<LightKanban> getLightKanbans();
+
+    void exportProfile(UUID lightUserId, String path);
+
+    void importMyProfile(String path);
+
+    void sendCreateProfile(List<?> profileDetails);
+}
