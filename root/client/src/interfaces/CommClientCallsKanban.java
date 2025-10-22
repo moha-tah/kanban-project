01589675199f9c351c813e9f69aaddb3216332a9
Théo Des <theophane.deschasse@etu.utc.fr>
2025-10-22 11:57:27 +0200

    import rajouter pour kanban

--- root/client/src/interfaces/CommClientCallsKanban.java
+++ root/client/src/interfaces/CommClientCallsKanban.java
@@ -1,3 +1,10 @@
+package root.client.src.interfaces;
+
+import root.common.src.dataClasses.Kanban;
+import root.common.src.dataClasses.Snapshot;
+import java.util.List;
+import java.util.UUID;
+
 // CommCallsKanban
 public interface CommClientCallsKanban {
     void deliverNotification(LightKanban idKanban, Modification modif);
--- root/client/src/interfaces/MainCallsKanban.java
+++ root/client/src/interfaces/MainCallsKanban.java
@@ -1,3 +1,12 @@
+
+package root.client.src.interfaces;
+
+import root.common.src.dataClasses.Kanban;
+import root.common.src.dataClasses.Snapshot;
+import java.util.List;
+import java.util.UUID;
+
+
 // MainCallsKanban
 public interface MainCallsKanban {
     void openCreateFrom();
