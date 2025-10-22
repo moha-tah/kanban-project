6b9f0648168057ede6f5eac0ce857fe4a9c8d88b
Martin <halbourg.martin@gmail.com>
2025-10-22 10:37:00 +0200

    [~] Move KanbanCallsDataClient

---
+++ root/client/src/interfaces/KanbanCallsDataClient.java
@@ -0,0 +1,12 @@
+package root.client.src.interfaces;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface KanbanCallsDataClient {
+    void saveSnapshot(Kanban kanban);
+    List<Snapshot> getListSnapshot();
+    Snapshot getSnapshot(Snapshot snap);
+    void deleteSnapshot(Snapshot snap);
+    void getModified(UUID modificationID, UUID kanbanID);
+}
