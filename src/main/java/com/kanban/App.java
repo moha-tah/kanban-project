ad2445b799bca04f21e6c9923aa93a1cd63a800e
Martin <halbourg.martin@gmail.com>
2025-11-05 11:04:30 +0100

    Structure du projet JavaFX avec Maven

---
+++ src/main/java/com/kanban/App.java
@@ -0,0 +1,31 @@
+package com.kanban;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Classe principale de l'application Kanban
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Kanban Project");
+
+        Label label = new Label("Bienvenue dans l'application Kanban !");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
