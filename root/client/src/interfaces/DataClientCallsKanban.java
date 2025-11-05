1d3315dc30df866bbcf0f103c11fc51ae6f746dc
Léo Lamy <leo.lamy@etu.utc.fr>
2025-11-05 11:24:35 +0100

    Copie du dossier interfaces depuis la branche V0

---
+++ root/client/src/interfaces/DataClientCallsKanban.java
@@ -0,0 +1,16 @@
+package root.client.src.interfaces;
+
+import root.common.src.dataClasses.Kanban;
+import root.common.src.dataClasses.Snapshot;
+import java.util.List;
+import java.util.UUID;
+
+// DataCallsKanban
+public interface DataClientCallsKanban {
+    void displayKanban(Kanban kanban);
+}
+
+
+
+
+
---
+++ root/client/src/interfaces/DataClientCallsMain.java
@@ -0,0 +1,15 @@
+package root.client.src.interfaces;
+
+import java.util.List;
+import java.util.UUID;
+import root.common.src.dataClasses.LightUser;
+import root.common.src.dataClasses.LightKanban;
+
+public interface DataClientCallsMain {
+    void updateListsKanbansUsers(List<LightKanban> kanbansOfUserDisconnected, UUID userId);
+    void updateListKanban(LightKanban lightKanban);
+    void addListModifiers(UUID kanbanId, UUID userId);
+    void uploadKanbans(UUID kanbanId);
+    void publishUsersList(List<LightUser> users);
+    void publishKanbansList(List<LightKanban> kanbans);
+}
