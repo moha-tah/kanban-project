fb6a184c5944fc065a2ec096bd49287beaa9714d
Léo Lamy <128738500+leolamy@users.noreply.github.com>
2025-11-14 21:31:48 +0100

    Feature connection comm ihmmain (#69)
    * fix implementation for compilation
    
    * create comm method for conncetion
    
    * login test

--- src/main/java/client/ihmMain/impl/commCallsMainImpl.java
+++ src/main/java/client/ihmMain/impl/commCallsMainImpl.java
@@ -5,6 +5,8 @@ import client.interfaces.CommClientCallsMain;
 import common.dataClasses.LightKanban;
 import common.dataClasses.LightUser;
 
+import java.util.List;
+
 /** Impl des callbacks de la couche Comm vers Main. */
 public class commCallsMainImpl implements CommClientCallsMain {
 
@@ -25,5 +27,33 @@ public class commCallsMainImpl implements CommClientCallsMain {
     public void displayPermissionRequest(LightUser user, LightKanban kanban) {
         System.out.println("[Main->CommCB] permission request from " + user.getUsername()
                 + " for kanban " + kanban.getTitle());
+    }
+
+    @Override
+    public void connectionAccepted(LightUser user, List<LightKanban> kanban) {
+        core.setMe(user);
+        core.addKanbans(kanban);
+        core.showHomeView();
+    }
+
+    @Override
+    public void connectServer(LightUser user, List<LightKanban> kanbans) {
+        if (user == null) {
+            System.err.println("[CommCallsMainImpl] connectServer: user is null");
+            return;
+        }
+
+        // 1) Mettre à jour l’état IHM
+        core.setMe(user);
+        if (kanbans != null && !kanbans.isEmpty()) {
+            core.addKanbans(kanbans);
+        }
+
+        // 2) Log pour debug
+        System.out.println(
+                "[CommCallsMainImpl] connectServer: user=" + user.getUsername()
+                        + ", kanbans=" + (kanbans == null ? 0 : kanbans.size())
+        );
+
     }
 }
