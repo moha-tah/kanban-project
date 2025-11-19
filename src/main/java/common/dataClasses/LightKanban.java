6110e1d5716d4c77a8c6a45e5df9809b6645098e
Engel <98811430+EngelCalon@users.noreply.github.com>
2025-11-19 12:53:41 +0100

    Fix dataclasses (#97)
    * Correction Kanban, Message, Task
    
    * [~] fix Kanban/Access
    
    * [+] Ajout du targetKanban dans les modifications
    
    * [fix] tentative fix sonarqube (sertialize)
    
    * [fix] Tasks
    
    * [fix] Access
    
    ---------
    
    Co-authored-by: Karim240502 <karim.abdelkafi@etu.utc.fr>

--- src/main/java/common/dataClasses/LightKanban.java
+++ src/main/java/common/dataClasses/LightKanban.java
@@ -1,11 +1,13 @@
 package common.dataClasses;
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 public class LightKanban implements Serializable {
     private static final long serialVersionUID = 1L;
     private String title;
     private UUID id;
+    private List<Access> accessList;
     
     // Constructeur
     public LightKanban(String title) {
@@ -27,6 +29,9 @@ public class LightKanban implements Serializable {
     public UUID getId() {
         return id;
     }
+    public List<Access> getAccessList() {
+        return accessList;
+    }
     
     // Setters
     public void setTitle(String title) {
@@ -36,4 +41,10 @@ public class LightKanban implements Serializable {
     public void setId(UUID id) {
         this.id = id;
     }
+    public void setAccessList(List<Access> accessList) {
+        this.accessList = accessList;
+    }
+    
+
+
 }
