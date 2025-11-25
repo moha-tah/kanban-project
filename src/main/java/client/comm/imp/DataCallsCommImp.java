220ddfd66f766fe78fd3c4dc7a24614a9423541c
Martin <halbourg.martin@gmail.com>
2025-11-25 20:21:33 +0100

    [~] Change SonarQube error

--- src/main/java/client/comm/imp/DataCallsCommImp.java
+++ src/main/java/client/comm/imp/DataCallsCommImp.java
@@ -41,9 +41,10 @@ public class DataCallsCommImp implements DataCallsComm {
     @Override
     public void addAuthorizedUser(UUID kanbanId, UUID userId) {
         try {
-            comm.sendMessage(Arrays.asList("addAuthorizedUser", kanbanId, userId));
+            commCore.sendMessage(java.util.Arrays.asList("addAuthorizedUser", kanbanId, userId));
         } catch (IOException e) {
-            e.printStackTrace();
+            java.util.logging.Logger.getLogger(DataCallsCommImp.class.getName())
+                    .log(java.util.logging.Level.SEVERE, "DataCallsCommImp: Echec envoi addAuthorizedUser", e);
         }
     }
 }
