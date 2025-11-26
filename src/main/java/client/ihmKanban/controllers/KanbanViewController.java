47fe582bf28a90215d22ecc9b8bf2e1dc41d508a
Tamkho-t <tdeschasse@gmail.com>
2025-11-26 11:26:41 +0100

    Fix affichage Users + correction layout Kanban + test mode UI (#122)
    * Fix affichage Users + correction layout Kanban + test mode UI
    
    * fix affichage Users + correction layout Kanban + test mode UI
    
    * Logger mis en place
    
    * changement 2
    
    ---------
    
    Co-authored-by: kenza-123 <kenza.fadouache@etu.utc.fr>
    Co-authored-by: Théo Des <theophane.deschasse@etu.utc.fr>
    Co-authored-by: moha-tah <moha.tah@icloud.com>
    Co-authored-by: ElieDSfrom <163874318+ElieDSfrom@users.noreply.github.com>

---
+++ src/main/java/client/ihmKanban/controllers/KanbanViewController.java
@@ -0,0 +1,46 @@
+package client.ihmKanban.controllers;
+
+import client.MainApp;
+import client.ihmMain.MainCore;
+import client.ihmMain.controllers.UsersController;
+import common.dataClasses.Column;
+import common.dataClasses.CreateTask;
+import common.dataClasses.LightKanban;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class KanbanViewController implements Initializable {
+
+    // contrôleur de users.fxml injecté grâce au fx:include fx:id="usersInclude"
+    @FXML
+    private UsersController usersIncludeController;
+
+    // contrôleur de displayKanban.fxml injecté grâce à fx:id="displayKanbanInclude"
+    @FXML
+    private DisplayKanbanController displayKanbanIncludeController;
+
+    private MainCore core;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        core = MainApp.getCore();
+
+        if (core != null && usersIncludeController != null) {
+            usersIncludeController.setCore(core);
+            usersIncludeController.refreshUsers();
+        }
+    }
+
+    public void initBoard(LightKanban kanban,
+                          List<Column> columns,
+                          List<CreateTask> taskCreations) {
+
+        if (displayKanbanIncludeController != null) {
+            displayKanbanIncludeController.initBoard(kanban, columns, taskCreations);
+        }
+    }
+}
