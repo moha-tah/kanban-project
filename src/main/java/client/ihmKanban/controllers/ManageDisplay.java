d5f74c341f1a53230c28b51ca628670de8f9adf9
Aabir Hamdaoui <aabir.hamdaoui@etu.utc.fr>
2025-11-27 12:35:33 +0100

    pull v2

--- src/main/java/client/ihmKanban/controllers/ManageDisplay.java
+++ src/main/java/client/ihmKanban/controllers/ManageDisplay.java
@@ -20,11 +20,7 @@ import java.util.logging.Level;
 
 public class ManageDisplay {
 
-<<<<<<< HEAD
-    private final kanbanCorps corps;
-=======
     private final kanbanCorps corps; 
->>>>>>> bf35d19 (fix)
 
     public ManageDisplay(kanbanCorps corps) {
         this.corps = corps;
