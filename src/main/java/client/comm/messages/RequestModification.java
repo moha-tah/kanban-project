732a915d1b3a42a63cdbef1e9b0935ee5ab36049
mbideaus <maxence.bideau-sorita@etu.utc.fr>
2025-12-03 13:09:00 +0100

    Fix sonarQ

--- src/main/java/client/comm/messages/RequestModification.java
+++ src/main/java/client/comm/messages/RequestModification.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import common.dataClasses.LightUser;
 import common.dataClasses.Modification;
-import server.ServerContext;
 import server.comm.CommCoreServer;
 
 /**
