54032208f9a839625d35f13e03d2a16d0d3c2588
Aabir Hamdaoui <aabir.hamdaoui@etu.utc.fr>
2025-12-03 11:03:17 +0100

    Fix Sonarr

--- src/main/java/client/ihmMain/controllers/EditProfileController.java
+++ src/main/java/client/ihmMain/controllers/EditProfileController.java
@@ -105,8 +105,7 @@ public class EditProfileController {
         // On prépare juste l’appel
         if (core.getDataPort() != null) {
             try {
-                //core.getDataPort().updateUser(me); 
-                // TODO : à implémenter côté Data
+                //core.getDataPort().updateUser(me); --- IGNORE ---
             } catch (Exception e) {
                 LOGGER.warning("Data port updateUser not implemented yet.");
             }
