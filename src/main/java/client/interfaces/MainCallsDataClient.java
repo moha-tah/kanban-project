7780f37bdd767805a9bbdfeb484c46184705675a
lucasdelbecque <157520700+lucasdelbecque@users.noreply.github.com>
2025-12-03 13:15:46 +0100

    Fix edit profile (#147)
    * edit profile avec methodes data
    
    * fix birthday picker
    
    * display name and profile picture
    
    * fix duplicate user
    
    * remove useless button
    
    * fix display profile
    
    ---------
    
    Co-authored-by: lucasdlbcq <lucas.delbecque78@icloud.com>
    Co-authored-by: Léo Lamy <leo.lamy@etu.utc.fr>

--- src/main/java/client/interfaces/MainCallsDataClient.java
+++ src/main/java/client/interfaces/MainCallsDataClient.java
@@ -1,10 +1,12 @@
 package client.interfaces;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import common.dataClasses.LightUser;
 import common.dataClasses.User;
 import common.dataClasses.LightKanban;
+import common.dataClasses.User;
 
 public interface MainCallsDataClient {
 
@@ -26,5 +28,7 @@ public interface MainCallsDataClient {
 
     void addAuthorizedUserToKanban(LightUser user, LightKanban kanban);
 
-    User getLocalUser();
+    User getLocalUser ();
+
+    Void modifyLocalUser (String newFirstName, String newLastName, LocalDate newBirthDate, String newAvatar, String newUsername);
 }
