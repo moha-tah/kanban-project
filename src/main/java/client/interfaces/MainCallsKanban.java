6c9fafdf69c6eaca049468f0804db785217d486f
elieDSfrom <eliedesolages@gmail.com>
2025-12-03 13:16:02 +0100

    ajout visualisation kanban depuis page profil. NE COMPILE PAS !!!

--- src/main/java/client/interfaces/MainCallsKanban.java
+++ src/main/java/client/interfaces/MainCallsKanban.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import client.ihmMain.controllers.HomeViewController;
+import client.ihmMain.controllers.ProfileViewController;
 import common.dataClasses.LightUser;
 import common.dataClasses.Kanban;
 
@@ -12,4 +13,6 @@ public interface MainCallsKanban {
     void displaySnapshotList();
     void openCreateForm(Kanban kanban, HomeViewController homeController);
 
+
+    void openKanbanViewFromProfile(Kanban kanban, ProfileViewController profileController);
 }
